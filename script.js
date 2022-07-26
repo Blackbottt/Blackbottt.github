dcffbdc2d1dcad6b54c672af4be71f00bdc4504f
Mufaro Machaya <sandmanmufmaf@gmail.com>
2022-07-26 18:54:08 +0200

    HTML structure and Javascript Links

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+    const facebook = document.getElementById('facebookLink');
+    const twitter = document.getElementById('twitterLink');
+    const snapchat = document.getElementById('snapchatLink');
+
+    facebook.setAttribute('href', 'facebook.com');
+    twitter.setAttribute('href', 'twitter.com');
+    snapchat.setAttribute('href', 'snapchat.com');
+}
